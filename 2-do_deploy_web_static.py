ee468a4c0854d5bc7e165f58d1784eca827c4ed9
Joshua Uzoagulu <joshua.uzoagulu@netapps.com.ng>
2024-03-19 13:50:50 +0100

    fix

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["34.232.70.826", "52.201.228.171"]
 env.user = "ubuntu"
 
 
